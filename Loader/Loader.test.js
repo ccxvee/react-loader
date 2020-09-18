cdb2e6575b570022f59b9ddbb5560c45ce41869f
ccxvee <ccxvee@mail.ru>
2020-09-19 07:52:50 +1100

    Update tests

--- Loader/Loader.test.js
+++ Loader/Loader.test.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { Loader } from "./Loader";
+import { render, screen, cleanup } from "@testing-library/react";
+import Loader from "./Loader";
+import { IDLE, LOADING, SUCCESS, ERROR } from "./statuses";
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -22,14 +22,305 @@ afterEach(() => {
   }
 });
 
-it("requires loadingStatus prop", () => {
-  expect(() =>
-    render(<Loader>child</Loader>)
-  ).toThrowErrorMatchingInlineSnapshot(`"requires loadingStatus prop"`);
+it("throws an error if status is undefined", () => {
+  expect(() => render(<Loader />)).toThrowErrorMatchingInlineSnapshot(
+    `"The status property must be one of the following string values: 'idle', 'loading', 'success' or 'error'."`
+  );
 
   console.error.mockClear();
 });
 
-it("does not require children", () => {
-  render(<Loader loadingStatus="idle" />);
+it("throws an error if status is not equal to IDLE, LOADING, SUCCESS or ERROR", () => {
+  expect(() =>
+    render(<Loader status={"unknown"} />)
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"The status property must be one of the following string values: 'idle', 'loading', 'success' or 'error'."`
+  );
+
+  console.error.mockClear();
+});
+
+it("requires only status prop", () => {
+  expect(() => render(<Loader status={IDLE} />));
+});
+
+it("returns idleView if status is equal to IDLE", () => {
+  render(
+    <Loader
+      status={IDLE}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(screen.getAllByText("idleView").length).toBe(1);
+});
+
+it("returns null if status is equal to IDLE and idleView is undefined", () => {
+  const { baseElement } = render(
+    <Loader
+      status={IDLE}
+      loadingView={"loadingView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+it("returns loadingView if status is equal to LOADING", () => {
+  render(
+    <Loader
+      status={LOADING}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(screen.getAllByText("loadingView").length).toBe(1);
+});
+
+it("returns null if status is equal to LOADING and loadingView is undefined", () => {
+  const { baseElement } = render(
+    <Loader
+      status={LOADING}
+      idleView={"idleView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+it("returns errorView if status is equal to ERROR", () => {
+  render(
+    <Loader
+      status={ERROR}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(screen.getAllByText("errorView").length).toBe(1);
+});
+
+it("returns null if status is equal to ERROR and errorView is undefined", () => {
+  const { baseElement } = render(
+    <Loader
+      status={ERROR}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={"successView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+it(`throws an error if 
+status is equal to SUCCESS 
+AND successView is defined 
+AND children is defined`, () => {
+  expect(() =>
+    render(
+      <Loader
+        status={SUCCESS}
+        idleView={"idleView"}
+        loadingView={"loadingView"}
+        successView={"successView"}
+        errorView={"errorView"}
+      >
+        children
+      </Loader>
+    )
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Only one of the following properties can be defined at the same time: 'successView' or 'children'."`
+  );
+
+  cleanup();
+
+  expect(() =>
+    render(
+      <Loader
+        status={SUCCESS}
+        idleView={"idleView"}
+        loadingView={"loadingView"}
+        successView={null}
+        errorView={"errorView"}
+      >
+        {null}
+      </Loader>
+    )
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Only one of the following properties can be defined at the same time: 'successView' or 'children'."`
+  );
+
+  cleanup();
+
+  expect(() =>
+    render(
+      <Loader
+        status={SUCCESS}
+        idleView={"idleView"}
+        loadingView={"loadingView"}
+        successView={0}
+        errorView={"errorView"}
+      >
+        {""}
+      </Loader>
+    )
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Only one of the following properties can be defined at the same time: 'successView' or 'children'."`
+  );
+
+  console.error.mockClear();
+});
+
+it(`returns successView if 
+status is equal to SUCCESS
+AND successView is defined
+AND children is undefined`, () => {
+  const { rerender } = render(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={"successView"}
+      errorView={"errorView"}
+    />
+  );
+
+  expect(screen.getAllByText("successView").length).toBe(1);
+
+  rerender(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={0}
+      errorView={"errorView"}
+    />
+  );
+
+  expect(screen.getAllByText("0").length).toBe(1);
+
+  cleanup();
+
+  const { baseElement } = render(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      successView={null}
+      errorView={"errorView"}
+    />
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+it(`returns children if 
+status is equal to SUCCESS 
+AND successView is undefined
+AND children is defined`, () => {
+  const { rerender } = render(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      errorView={"errorView"}
+    >
+      children
+    </Loader>
+  );
+
+  expect(screen.getAllByText("children").length).toBe(1);
+
+  rerender(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      errorView={"errorView"}
+    >
+      {0}
+    </Loader>
+  );
+
+  expect(screen.getAllByText("0").length).toBe(1);
+
+  cleanup();
+
+  const { baseElement } = render(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      errorView={"errorView"}
+    >
+      {null}
+    </Loader>
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+it(`returns null if 
+status is equal to SUCCESS
+AND successView is undefined
+AND children is undefined`, () => {
+  const { baseElement } = render(
+    <Loader
+      status={SUCCESS}
+      idleView={"idleView"}
+      loadingView={"loadingView"}
+      errorView={"errorView"}
+    />
+  );
+
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
 });
